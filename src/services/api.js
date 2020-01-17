4d3cf2bcb67e8a0c014cc840d5e906a27d6d10e1
Rodrigo Pauletti <digopauletti@yahoo.com.br>
2020-01-17 09:43:44 -0300

    Remove console.log

--- src/services/api.js
+++ src/services/api.js
@@ -12,43 +12,22 @@ api.interceptors.request.use(config => {
   if (token) {
     try {
       const decoded = jwtDecode(token);
-      console.log("token valid");
       const date = new Date(0);
       if (date.setUTCSeconds(decoded.exp) < Date.now()) {
-        console.log("token expirated | prevent request");
         return redirectToLogin();
       }
-      console.log("token not expirated | continue");
     } catch (err) {
-      console.log("invalid token | prevent request");
       return redirectToLogin();
     }
     const tkn = `Bearer ${token}`;
     config.headers.Authorization = tkn;
   } else {
     if (!config.data || !config.data.withoutToken) {
-      console.log("without token | prevent request");
       return redirectToLogin();
     }
   }
-  console.log("continuing...");
-
   return config;
 });
-
-api.interceptors.response.use(
-  res => {
-    // console.log("RES", res);
-    return res;
-  }
-  // err => {
-  //   console.log("err");
-  //   console.log(err);
-  //   return err;
-  //   // console.log(error.toJSON());
-  //   // return redirectToLogin();
-  // }
-);
 
 export default api;
 
